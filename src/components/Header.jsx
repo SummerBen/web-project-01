c0cf48352ab647b49b8aeb47717289f5be96feca
unknown <kuznetsov.nikolay97@gmail.com>
2021-12-17 21:35:30 +0300

    1st part init rep

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/505px-Apple_logo_black.svg.png' />
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+    return  <div className='content'>
+    <div>
+      <img src='https://images.ctfassets.net/hrltx12pl8hq/euxCffMOPuxAnPLcN3nzW/eb14f1deaa1e6edce8981124825aefb9/ULOHP.png?fit=fill&w=800&h=400' />
+    </div>
+    <div>
+      avatarka + description
+    </div>
+    <div>
+      My posts
+    </div>
+    <div>
+      New post
+    </div>
+    <div>
+      Post 1
+    </div>
+    <div>
+      Post 2
+    </div>
+  </div>
+}
+
+export default Profile;
