8c113c50cddd2477a786ec71d70b01bb9802ceae
unknown <kuznetsov.nikolay97@gmail.com>
2022-01-03 20:44:13 +0300

    Lesson 21 done

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,29 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Messages
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Keks
+                </div>
+                <div className={s.dialog}>
+                    Marina
+                </div>
+                <div className={s.dialog}>
+                    Diman
+                </div>
+                <div className={s.dialog}>
+                    Andrew
+                </div>
+                <div className={s.dialog}>
+                    Nixxxon
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>С новым годом, гнида!</div>
+                <div className={s.message}>Слава роду славянскому!</div>
+                <div className={s.message}>Слава Украине!</div>
+            </div>
         </div>
     )
 }
