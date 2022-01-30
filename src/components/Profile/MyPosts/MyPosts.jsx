3c29e8be8309bdae5951fa931b5c2b67832bec1f
unknown <kuznetsov.nikolay97@gmail.com>
2022-01-30 23:41:02 +0300

    Lesson 31 done (При добавлении поста текст отображается в alert)

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,19 +1,27 @@
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+import React from "react";
 
 const MyPosts = (props) => {
 
   let postsElements = props.postsData.map(p => <Post message={p.message} likesCount={p.likesCount} dislikesCount={p.dislikesCount} />)
+
+  let newPostsElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostsElement.current.value;
+    alert(text);
+  }
 
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostsElement}></textarea>
         </div>
         <div>
-          <button>Add Post</button>
+          <button onClick={addPost}>Add Post</button>
         </div>
       </div>
       <div className={s.posts}>
