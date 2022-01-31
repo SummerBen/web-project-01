aa1b654f4a0c27d2ef0b8f79b73a49a216df6599
unknown <kuznetsov.nikolay97@gmail.com>
2022-01-31 19:24:28 +0300

    Lesson 31 homework (Добавил отображение текста сообщения в alert)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,19 @@
 import DialogItem from './DialogItem/DialogItem';
 import s from './Dialogs.module.css';
 import Message from './Message/Message';
+import React from "react";
 
 const Dialogs = (props) => {
 
     let dialogsElements = props.state.dialogsData.map(d => <DialogItem name={d.name} id={d.id} avatar={d.avatar} />)
-    let messagesElements = props.state.messagesData.map(m => <Message message={m.message} myMessage={m.myMessage} avatar={m.avatar}/>)
+    let messagesElements = props.state.messagesData.map(m => <Message message={m.message} myMessage={m.myMessage} avatar={m.avatar} />)
+
+    let newMessageElement = React.createRef();
+
+    let sendMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    }
 
     return (
         <div className={s.dialogs}>
@@ -14,6 +22,10 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+                <textarea ref={newMessageElement}></textarea>
+                <div>
+                    <button onClick={sendMessage}>Send</button>
+                </div>
             </div>
         </div>
     )
